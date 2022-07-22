53f7f8bfe04be06d0d6454bd1ce4648d03080e2f
Jonathan Bartram <jonny.ou@ntlworld.com>
2022-07-22 07:53:30 +0100

    badges4

--- src/components/previewcard/preview-card.component.jsx
+++ src/components/previewcard/preview-card.component.jsx
@@ -1,4 +1,4 @@
-import { Card , Button, CardImg} from "react-bootstrap";
+import { Card , Button} from "react-bootstrap";
 import './preview-card.style.css';
 import { CatContext } from "../../contexts/cat-context/cat-context.context";
 import { useContext } from "react";
--- src/routes/home/home.route.jsx
+++ src/routes/home/home.route.jsx
@@ -2,17 +2,22 @@ import { Container } from "react-bootstrap";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faMartiniGlass, faBeerMugEmpty} from '@fortawesome/free-solid-svg-icons';
+import {  faMartiniGlass, faBeerMugEmpty, faChampagneGlasses} from '@fortawesome/free-solid-svg-icons';
 import './home.style.css'
 const Home = (props) =>{
     return(
-        <div className='text-center fillScreen white-t'>
-            <Container className='mt-5' >
-           < Row lg = {1} md={1} sm={1} xs={1} className='max-w800'>
-           <Col><h1 className='mb-5'>Fresh Drink Ideas</h1></Col>
+        <div >
+            <div className='banner'>
+            < Row lg = {2} md={2} sm={1} xs={1} className='max-w800'>
+            <Col><h1 className='banner-text'>Fresh drink ideas just a click away!</h1></Col>
+            </Row>
+            </div>
+            <Container className='mt-5 text-center' >
+           < Row lg = {3} md={3} sm={3} xs={3} className='max-w800'>
+           
             <Col><FontAwesomeIcon icon={faMartiniGlass} size='lg' className='big-icon white-t'/></Col>
-            <Col><h1>Just a tap away!</h1></Col>
-            <Col><FontAwesomeIcon icon={faBeerMugEmpty} size='lg' className='big-icon white-t mt-5'/></Col>
+            <Col><FontAwesomeIcon icon={faBeerMugEmpty} size='lg' className='big-icon white-t'/></Col>
+            <Col><FontAwesomeIcon icon={faChampagneGlasses} size='lg' className='big-icon white-t'/></Col>
            </Row>
            </Container>
            </div>
