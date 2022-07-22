a73e47a3d0051182b1c381dd64d8959da07d0b9b
Jonathan Bartram <jonny.ou@ntlworld.com>
2022-07-22 11:06:31 +0100

    readme

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,7 +4,11 @@ const MainFooter = () =>{
         <footer className="bottom-container mt-auto">
         <a className="footer-link" href="https://www.linkedin.com/in/jonathan-bartram-b98b382b/">LinkedIn</a>
         <a className="footer-link" href="https://www.facebook.com/jon.bartram/">Facebook</a>
+        <a className="footer-link" href="https://www.thecocktaildb.com/api.php?ref=apilist.fun">The Cocktail db</a>
+
+        
         <p>© Jonathan Bartram.</p>
+        <p>Data provided by the cocktail db.</p>
       </footer>
     )
 }
