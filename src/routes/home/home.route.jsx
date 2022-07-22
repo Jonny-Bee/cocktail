5fd74256c3a151ab86b732992f096e011a5b08c0
Jonathan Bartram <jonny.ou@ntlworld.com>
2022-07-22 08:12:45 +0100

    badges5

--- src/routes/home/home.route.jsx
+++ src/routes/home/home.route.jsx
@@ -12,15 +12,8 @@ const Home = (props) =>{
             <Col><h1 className='banner-text'>Fresh drink ideas just a click away!</h1></Col>
             </Row>
             </div>
-            <Container className='mt-5 text-center' >
-           < Row lg = {3} md={3} sm={3} xs={3} className='max-w800'>
            
-            <Col><FontAwesomeIcon icon={faMartiniGlass} size='lg' className='big-icon white-t'/></Col>
-            <Col><FontAwesomeIcon icon={faBeerMugEmpty} size='lg' className='big-icon white-t'/></Col>
-            <Col><FontAwesomeIcon icon={faChampagneGlasses} size='lg' className='big-icon white-t'/></Col>
-           </Row>
-           </Container>
-           </div>
+        </div>
         
        
     )
